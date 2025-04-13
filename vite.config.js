eaeff768d36ee406740562f09b9dcf289e779b0f
shala190035 <urimshala481@gmail.com>
2025-04-13 16:02:23 +0200

    update

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,16 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/'
+  base: '',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        entryFileNames: `assets/[name].js`,
+        chunkFileNames: `assets/[name].js`,
+        assetFileNames: `assets/[name].[ext]`
+      }
+    }
+  }
 })
